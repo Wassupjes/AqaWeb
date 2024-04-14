46168ca11dea726b7dfb8778483fcf6dd345b445
Alexey <wassupjes@mail.ru>
2024-04-14 23:46:24 +0300

    Added positive test #1

--- src/test/java/ru/netology/web/DebitCardApplication.java
+++ src/test/java/ru/netology/web/DebitCardApplication.java
@@ -1,6 +1,5 @@
 package ru.netology.web;
 
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -8,10 +7,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -26,7 +26,11 @@ class DebitCardApplication {
 
     @BeforeEach
     void setUp() {
-    driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
@@ -38,17 +42,13 @@ class DebitCardApplication {
     @Test
     void shouldDebitCardApplicationPositive() throws InterruptedException {
         driver.get("http://localhost:7777");
-        WebElement name = driver.findElement(By.cssSelector("[data-test-id=name]"));
-        WebElement phone = driver.findElement(By.cssSelector("[data-test-id=phone]"));
-        WebElement buttonAgreement = driver.findElement(By.cssSelector("[data-test-id=agreement]"));
-        WebElement buttonRole = driver.findElement(By.cssSelector("[role=button]"));
-        name.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Владимиров Василий");
-        phone.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79888888888");
-        buttonAgreement.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        buttonRole.findElement(By.cssSelector("[role=button]")).click();
-        String text = driver.findElement(By.className("[data-test-id=order-success]")).getText();
+        driver.findElement(By.cssSelector("[data-test-id=name]")).sendKeys("Владимиров Василий");
+        driver.findElement(By.cssSelector("[data-test-id=phone]")).sendKeys("+79888888888");
+        driver.findElement(By.className("checkbox__control")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("Success")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время", text.trim());
-        Thread.sleep(5000);
+        Thread.sleep(50000);
     }
     }
 
