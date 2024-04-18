7a7f36b62e592f7536e12e05a45965064b9860f1
Alexey <wassupjes@mail.ru>
2024-04-18 22:53:40 +0300

    Added negative tests

--- src/test/java/ru/netology/web/DebitCardApplication.java
+++ src/test/java/ru/netology/web/DebitCardApplication.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -45,10 +46,11 @@ class DebitCardApplication {
         driver.findElement(By.className("button")).click();
         String actualText = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
-        Thread.sleep(5000);
     }
+
     @Test
-    void invalidValueOfEnteringLastAndFirstName() throws InterruptedException {
+        // ввод латинских букв в поле фамилия и имя
+    void enteringLatinLetters() throws InterruptedException {
         driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Vladimirov Vasiliy");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79888888888");
@@ -56,8 +58,102 @@ class DebitCardApplication {
         driver.findElement(By.className("button")).click();
         String actualText = driver.findElement(By.className("input__sub")).getText();
         assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", actualText);
-        Thread.sleep(5000);
-        }
     }
+
+    @Test
+    void emptyLastNameAndFirstNameField() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79888888888");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String actualText = driver.findElement(By.className("input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", actualText);
+    }
+
+    @Test
+    // ввод спецсимволов в поле фамилия и имя
+    void enteringSpecialCharacters() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Владимиров/ Василий");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79888888888");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String actualText = driver.findElement(By.className("input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", actualText);
+    }
+
+
+    @Test
+        // ввод имени с буквой ё в поле фамилия и имя
+    void invalidLetter() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Владимиров Семён");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79888888888");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String actualText = driver.findElement(By.className("input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", actualText);
+    }
+
+    @Test
+    // ввод 11значного номера без +
+    void invalidPhoneEntryFirst() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Владимиров Василий");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("79888888888");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String actualText = driver.findElement(By.className("input__sub")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", actualText);
+    }
+
+    @Test
+        // ввод 12значного номера
+    void invalidPhoneEntrySecond() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Владимиров Василий");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+798888888888");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String actualText = driver.findElement(By.className("input__sub")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", actualText);
+    }
+
+    @Test
+        // ввод букв в поле номера
+    void invalidPhoneEntryThird() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Владимиров Василий");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+798888888аа");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String actualText = driver.findElement(By.className("input__sub")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", actualText);
+    }
+
+    @Test
+        // пустое поле номера
+    void invalidPhoneEntryFourth() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Владимиров Василий");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String actualText = driver.findElement(By.className("input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", actualText);
+    }
+
+    @Test
+        // не проставлена галочка напротив строки Согласия на обработку данных
+    void submittingFormWithoutConsent() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Владимиров Василий");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79888888888");
+        driver.findElement(By.className("button")).click();
+        String actualText = driver.findElement(By.className("checkbox__text")).getText();
+        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", actualText);
+    }
+}
 
 
